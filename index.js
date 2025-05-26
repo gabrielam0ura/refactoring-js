be2340f4ab0a2cf1b20210008ea7bd92acdedd43
gabrielam0ura <gabrielamoura@alu.ufc.br>
2025-05-26 10:54:01 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ function gerarFaturaStr(fatura, pecas) {
                       { style: "currency", currency: "BRL",
                         minimumFractionDigits: 2 }).format;
 
-  // Função extraída
+  function getPeca(apresentacao) {
+      return pecas[apresentacao.id];
+    }
+
   function calcularTotalApresentacao(apre, peca) {
     let total = 0;
-    switch (peca.tipo) {
+    switch (getPeca(apre).tipo) {
       case "tragedia":
         total = 40000;
         if (apre.audiencia > 30) {
@@ -32,16 +35,16 @@ function gerarFaturaStr(fatura, pecas) {
   }
 
   for (let apre of fatura.apresentacoes) {
-    const peca = pecas[apre.id];
-    let total = calcularTotalApresentacao(apre, peca);
+    // const peca = getPeca(apre);
+    let total = calcularTotalApresentacao(apre, getPeca(apre));
 
     // créditos para próximas contratações
     creditos += Math.max(apre.audiencia - 30, 0);
-    if (peca.tipo === "comedia") 
+    if (getPeca(apre).tipo === "comedia") 
       creditos += Math.floor(apre.audiencia / 5);
 
     // mais uma linha da fatura
-    faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+    faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
     totalFatura += total;
   }
 
